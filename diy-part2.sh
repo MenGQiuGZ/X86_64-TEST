e48fc4cfa8fe51e00391c9c947111bc7c66094e3
smdx <smdx02@gmail.com>
2022-03-15 11:15:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -33,13 +33,13 @@ git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/luci-app-
 
 
 # themes添加（svn co 命令意思：指定版本如https://github）
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/luci-theme-infinityfreedom
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
 # Add luci-theme-argon
 #git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
 
-#git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
+git clone https://github.com/kiddin9/openwrt-bypass.git package/luci-app-bypass
 
 #git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 
